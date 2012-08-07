63dc3368a9012209674d9290d579f605ee20b405
Andrew Garcia <agarcia@presidio.com>
2012-08-07 10:06:06 -0400

    added optionPanel

--- gui/OptionPanel.java
+++ gui/OptionPanel.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
@@ -19,10 +20,8 @@ public class OptionPanel implements ItemListener
 	{
 		layout = new GridLayout();
 		panel = new JPanel(layout);
-		panel.setSize(10, 10);
 		num_rows = 1;
 		selectedOptions = new ArrayList<String>();
-
 	}
 
 	public JPanel getPanel()
@@ -32,17 +31,36 @@ public class OptionPanel implements ItemListener
 
 	public void add(JCheckBox option)
 	{
+		option.setBackground(Color.WHITE);
 		++num_rows;
 		layout.setRows(num_rows);
 		panel.setLayout(layout);
 		panel.add(option);
 		option.addItemListener(this);
+		panel.setSize(panel.getPreferredSize());
 	}
+	
+//	public void addOtherField()
+//	{
+//		JCheckBox other = new JCheckBox("Other");
+//		other.setBackground(Color.WHITE);
+//		panel.add(other);
+//		++num_rows;
+//		layout.setRows(num_rows);
+//		panel.setLayout(layout);
+//		JTextArea input = new JTextArea(2, 60);
+//		input.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+//		panel.add(input);
+//	}
 
 	public void clear()
 	{
+		num_rows = 1;
+		layout.setRows(num_rows);
 		panel.removeAll();
 		selectedOptions.clear();
+		panel.setLayout(layout);
+		panel.setSize(panel.getPreferredSize());
 	}
 	
 	public ArrayList<String> getSelectedOptions()
